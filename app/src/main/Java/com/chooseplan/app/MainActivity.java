006249cd346bb901ef8b4aba90c89bbcc132a663
alannaics <alannaisabellecs@gmail.com>
2026-08-16 17:32:08 -0300

    Add files via upload

---
+++ app/src/main/Java/com/chooseplan/app/MainActivity.java
@@ -0,0 +1,26 @@
+package com.chooseplan.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient());
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(false);
+
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+}
